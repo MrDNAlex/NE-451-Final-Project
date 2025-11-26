c125e2d96cf1d93c9500f60c497b390c0881bedf
MrDNAlex <mr.dnalex.2003@gmail.com>
2025-11-26 10:16:55 -0500

    Initial Commit and Convergence Test

---
+++ ConvergenceTest/Attempt1/run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --job-name=FP-conv-test1
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --time=3:00:00
+#SBATCH --mem-per-cpu=600M 
+#SBATCH --cpus-per-task=12  # 1 thread per rank
+
+module load class-simulations
+
+mpirun -np $SLURM_NTASKS pw.x < test.scf.in > test.scf.out
