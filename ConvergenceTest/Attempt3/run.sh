489cacb7170a8e9c73757adb4cade190ed69a4e6
MrDNAlex <mr.dnalex.2003@gmail.com>
2025-11-26 11:10:25 -0500

    Increased Memory and ready to run

--- ConvergenceTest/Attempt3/run.sh
+++ ConvergenceTest/Attempt3/run.sh
@@ -3,7 +3,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --time=3:00:00
-#SBATCH --mem-per-cpu=600M 
+#SBATCH --mem-per-cpu=1000M 
 #SBATCH --cpus-per-task=12  # 1 thread per rank
 
 module load class-simulations
